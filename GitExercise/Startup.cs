12aff49b106ea0b17eb91836fc01dab03918a06d
irinapetrova15 <ir.petrova14@gmail.com>
2025-10-22 12:41:19 +0300

    Division, Abs Subs added

--- GitExercise/Startup.cs
+++ GitExercise/Startup.cs
@@ -4,9 +4,26 @@ namespace GitExercise
 {
     public class Startup
     {
+        
+        private static bool CheckCredentials()
+        {
+            Console.Write("Enter password to gain access: ");
+            string password = Console.ReadLine();
+            Console.Clear();
+            return password == Password;
+        }
+        private const string Password = "abcd1234";
         public static void Main()
         {
-            Console.WriteLine("Console Calculator App");
+            bool isAuthorized = CheckCredentials();
+            if (!isAuthorized)
+            {
+                Console.WriteLine("Access denied.");
+                Console.ReadKey(intercept: true);
+                return;
+            }
+            
+                    Console.WriteLine("Console Calculator App");
             Console.WriteLine(new string('-', 15));
 
             Console.Write("a = ");
@@ -34,6 +51,25 @@ namespace GitExercise
                     break;
                 case "m":
                     OptionsManager.Multiply(a, b);
+                    break;
+            }
+
+            switch (choice)
+            {
+                case "a":
+                    OptionsManager.Add(a, b);
+                    break;
+                case "s":
+                    OptionsManager.Subtract(a, b);
+                    break;
+                case "m":
+                    OptionsManager.Multiply(a, b);
+                    break;
+                case "d":
+                    OptionsManager.Divide(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubtractAbs(a, b);
                     break;
             }
 
